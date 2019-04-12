f9e2b0b28ada77e7b432f3fa6ca1cd63f9175623
Rajasekar <relango@salesforce.com>
2019-04-11 23:02:29 -0400

    WIP first version

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,76 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest" target="_blank" rel="noopener">unit-jest</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div class="hello" id="editor">
+        <textarea :value="input" @input="update"></textarea>
+        <div v-html="compiledMarkdown"></div>
+    </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+    import Vue from 'vue';
+    import { Marked } from 'marked-ts';
+  
+    export default Vue.extend({
+        name: 'HelloWorld',
+        data() {
+            return {
+                input: '# hello',
+            };
+        },
 
-@Component
-export default class HelloWorld extends Vue {
-  @Prop() private msg!: string;
-}
+        methods: {
+           update(e: any) {
+                this.input = e.target.value;
+            },
+
+        },
+        computed: {
+            compiledMarkdown(): string {
+                return Marked.parse(this.input);
+            },
+        },
+    });
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="stylus">
-h3
-  margin 40px 0 0
+    h3
+        margin 40px 0 0
 
-ul
-  list-style-type none
-  padding 0
+    ul
+        list-style-type none
+        padding 0
 
-li
-  display inline-block
-  margin 0 10px
+    li
+        display inline-block
+        margin 0 10px
 
-a
-  color #42b983
+    a
+        color #42b983
+
+    #editor
+        margin 0
+        height 100%
+        font-family 'Helvetica Neue', Arial, sans-serif
+        color #333
+
+    textarea, #editor div
+        display inline-block
+        width 49%
+        height 100%
+        vertical-align top
+        box-sizing border-box
+        padding 0 20px
+
+    textarea
+        border none
+        border-right 1px solid #ccc
+        resize none
+        outline none
+        background-color #f6f6f6
+        font-size 14px
+        font-family 'Monaco', courier, monospace
+        padding 20px
+
+    code
+        color #f66
 </style>
